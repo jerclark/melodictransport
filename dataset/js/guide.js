12fc3673da919f8be6cf7f8e8f70038d5f4ca0b4
Bruno Carriere <neonskimmer@gmail.com>
2016-04-03 19:30:40 -0400

    Dataset explorer more stuff

--- dataset/js/guide.js
+++ dataset/js/guide.js
@@ -4,29 +4,29 @@ function tag(t, value) {
     return "<" + t + ">" + value + "</" + t + ">";
 }
 
+var th = _.partial(tag, "th");
+var tr = _.partial(tag, "tr");
+var td = _.partial(tag, "td");
+var tbody = _.partial(tag, "tbody");
+var thead = _.partial(tag, "thead");
+var table = _.partial(tag, "table");
+
 // Take an array of objects and output a table element. filterKeys is a
 // predicate function to filter which keys (e.g. columns) get show.
-function toTable(arr, filterKeys) {
+function toTable(arr, keys, idField) {
     if (arr.length === 0) return tag("table", "");
+    var toString = function(arr){ return arr.join("") };
 
-    var filterKeys = filterKeys || function(){ return true; };
-    var fieldnames = Object.keys(arr[0]).filter(filterKeys);
-    var header =
-        tag("thead",
-        tag("tr", fieldnames.map(_.partial(tag, "th")).join("")));
+    keys = keys || _.keys(arr[0]);
+    arr = arr.map(_.partial(_.pick, _, keys));
 
-    var values = function(d) {
-        return fieldnames.reduce(function(v, k) {
-            v.push(d[k]);
-            return v;
-        }, []);
-    };
+    var header = thead(tr(keys.map(th).join("")));
 
-    var body =
-        tag("tbody", arr
-            .map(function(d) {
-                return tag("tr", values(d).map(_.partial(tag, "td")).join(""))
-            }).join(""));
+    // fine line between clever and stupid
+    var body = tbody(arr
+        .map(_.values)
+        .map(_.partial(_.map, _, td)).map(toString)
+        .map(tr).join(""));
 
     return tag("table class='table table-hover table-condensed table-selectable'", header + body);
 }
@@ -38,37 +38,58 @@ function filterBy(field, value) {
     };
 }
 
-function containing(fields) {
-    return _.partial(_.contains, fields);
-}
-
 $(function() {
     queue()
         .defer(d3.tsv, "raw/cx/cx.demographics")
         .defer(d3.tsv, "raw/cx/cx.characteristics")
         .await(showDemo);
 
-    function showDemo(errors, demographics, characteristics) {
-        console.log(demographics);
-        console.log(characteristics);
+    function drilldownTable(options) {
+        var parentData = options.parentData;
+        var parentFields = options.parentFields;
+        var childrenData = options.childrenData;
+        var childrenFields = options.childrenFields;
+        var idLocator = options.idLocator;
+        var idField = options.idField;
 
-        var demoFields = ["demographics_code", "demographics_text"];
-        var charFields = ["demographics_code", "characteristics_code", "characteristics_text"];
+        var parentTable = $(toTable(parentData, parentFields));
+        parentTable.addClass("table-parent");
+        parentTable.find("tbody tr").on("click", function(e) {
+            var id = idLocator(e.currentTarget);
+            var filteredData = childrenData.filter(function(d) {
+                return d[idField] === id;
+            });
+            var subtable = toTable(filteredData, childrenFields);
 
-        $("#demographics").html(
-            toTable(demographics, containing(demoFields)));
-
-        $("#demographics").on("click", "tr", function(e) {
-            var dem_code = $(e.currentTarget).children("td").first().text();
-
-            $(e.currentTarget).siblings().removeClass("selected");
-            $(e.currentTarget).addClass("selected");
-
-            $("#characteristics").html(toTable(characteristics.filter(function(d) {
-                return d.demographics_code === dem_code;
-            }), _.partial(_.contains, charFields)))
+            parentTable.find(".subtable").remove();
+            $('<tr class="subtable"><td colspan="' + parentFields.length + '">'
+                + subtable + '</td></tr>')
+                .insertAfter(e.currentTarget);
         });
 
+        return parentTable;
+    }
+
+    // Handle selectable tables
+    $("body").on("click", ".table-selectable tbody tr", function(e) {
+        $(e.currentTarget).siblings().removeClass("selected");
+        $(e.currentTarget).addClass("selected");
+        $(e.currentTarget).trigger("row-selected", e.currentTarget);
+    });
+
+    function showDemo(errors, demographics, characteristics) {
+        var table = drilldownTable({
+            parentData: demographics,
+            parentFields: ["demographics_code", "demographics_text"],
+            childrenFields: ["characteristics_code", "characteristics_text"],
+            childrenData: characteristics,
+            idLocator: function(tr) {
+                return $(tr).children("td").first().text();
+            },
+            idField: "demographics_code"
+        });
+
+        $("#demographics").empty().append(table);
         $("#demographics tbody tr").first().click();
     }
 
@@ -78,21 +99,24 @@ $(function() {
         .await(showItems)
 
     function showItems(errors, subcategory, items) {
-        console.log(subcategory, items);
-        var catFields = ["category_code", "subcategory_code", "subcategory_text"];
-        var itemFields = ["subcategory_code", "item_code", "item_text"];
-
-        $("#subcategories").html(toTable(subcategory, containing(catFields)));
-        $("#subcategories").on("click", "tr", function(e) {
-            var sub_code = $(e.currentTarget).children("td").eq(1).text();
-            $(e.currentTarget).siblings().removeClass("selected");
-            $(e.currentTarget).addClass("selected");
-
-            $("#items").html(
-                toTable(items.filter(filterBy("subcategory_code", sub_code)), containing(itemFields)));
+        var table = drilldownTable({
+            parentData: subcategory,
+            parentFields:  ["category_code", "subcategory_code", "subcategory_text"],
+            childrenData: items,
+            childrenFields: ["subcategory_code", "item_code", "item_text"],
+            idLocator: function(tr) {
+                return $(tr).children("td").eq(1).text();
+            },
+            idField: "subcategory_code"
         });
 
+        $("#subcategories").empty().append(table);
         $("#subcategories tbody tr").first().click();
     }
 
+    queue()
+        .defer(d3.tsv, "raw/cx/cx.series")
+        .await(function(errors, series) {
+            console.log("series loaded", series);
+        });
 });
